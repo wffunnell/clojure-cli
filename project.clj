cf035b04dbc849ff1764ddb01378f69ede416244
Will Funnell <w.f.funnell@gmail.com>
2014-05-05 23:39:38 +0100

    Added embedded cassandra DB with config Added logging

--- project.clj
+++ project.clj
@@ -4,5 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.cli "0.3.1"]]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [clojurewerkz/cassaforte "1.3.0-beta12"]
+                 [org.apache.cassandra/cassandra-all "2.0.2" :scope "test"]
+                 [org.clojure/tools.logging "0.2.6"]]
   :main clojure-cli.bootstrap)
--- test/clojure_cli/bootstrap_test.clj
+++ test/clojure_cli/bootstrap_test.clj
@@ -1,10 +1,25 @@
-(ns clojure-cli.bootstrap_test
+(ns clojure-cli.cass_tools_test
   (:require [clojure.test :refer :all]
-            [clojure-cli.bootstrap :as app]))
-;
+            [clojure-cli.cass-tools :as app]
+            [clojurewerkz.cassaforte.embedded :as cass]
+            [clojurewerkz.cassaforte.client :as client]))
+
+(defn spin-cassandra []
+  (cass/start-server! :cleanup true))
+
+;(client/connect! ["127.0.0.1"])
+
+(defn kill-cassandra []
+  (cass/stop-server!))
+
+(use-fixtures :once
+              (fn [your-tests]
+                (spin-cassandra)
+                (your-tests)
+                (kill-cassandra)))
+
 ;(deftest bootstrap-with-no-args
 ;  (testing "run application with no arguments displays help section"
-;    (is (= "Help section" (app/-main)))))
-;
+;    (is (= 1 1))))
 
 
